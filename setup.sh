39e4c301f6cbad9b29acfa6112b846f21b5cbba4
ssleert <semenilichevsky@gmail.com>
2022-03-02 01:53:33 +0300

    optimize

--- setup.sh
+++ setup.sh
@@ -10,18 +10,5 @@ case "$1" in
 esac
 cp conf/picom.conf $HOME/.config/
 sudo make clean install
-sudo touch /usr/share/xsessions/sfowm.desktop
-sudo echo "[Desktop Entry]" >> /usr/share/xsessions/sfowm.desktop
-sudo echo "Encoding=UTF-8" >> /usr/share/xsessions/sfowm.desktop
-sudo echo "Name=sfowm" >> /usr/share/xsessions/sfowm.desktop
-sudo echo "Comment=useless fork of dwm" >> /usr/share/xsessions/sfowm.desktop
-sudo echo "Exec=sfowm" >> /usr/share/xsessions/sfowm.desktop
-sudo echo "Icon=sfowm" >> /usr/share/xsessions/sfowm.desktop
-sudo echo "Type=XSession" >> /usr/share/xsessions/sfowm.desktop
-sudo chmod 777 /usr/share/xsessions/sfowm.desktop
-mkdir $HOME/.sfowm
-touch $HOME/.sfowm/autostart.sh
-echo "#!/bin/sh" >> $HOME/.sfowm/autostart.sh
-echo "picom" >> $HOME/.sfowm/autostart.sh
-chmod +x $HOME/.sfowm/autostart.sh
-
+sudo cp conf/sfowm.desktop /usr/share/xsessions/sfowm.desktop
+cp conf/.sfowm $HOME/.sfowm
