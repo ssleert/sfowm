e6faa2aa30dfc321d6389afb1a00d9b1b612623d
ssleert <semenilichevsky@gmail.com>
2022-03-02 00:05:08 +0300

    add xsession file

--- setup.sh
+++ setup.sh
@@ -11,3 +11,12 @@ case "$1" in
 esac
 cp picom.conf $HOME/.config/
 sudo make clean install
+sudo touch /usr/share/xsessions/sfowm.desktop
+sudo echo "[Desktop Entry]" >> /usr/share/xsessions/sfowm.desktop
+sudo echo "Encoding=UTF-8" >> /usr/share/xsessions/sfowm.desktop
+sudo echo "Name=sfowm" >> /usr/share/xsessions/sfowm.desktop
+sudo echo "Comment=useless fork of dwm" >> /usr/share/xsessions/sfowm.desktop
+sudo echo "Exec=sfowm" >> /usr/share/xsessions/sfowm.desktop
+sudo echo "Icon=sfowm" >> /usr/share/xsessions/sfowm.desktop
+sudo echo "Type=XSession" >> /usr/share/xsessions/sfowm.desktop
+sudo chmod 777 /usr/share/xsessions/sfowm.desktop
