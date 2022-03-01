e8b7527315db58c038d3ea9778134e764bad7c89
ssleert <semenilichevsky@gmail.com>
2022-03-02 02:29:43 +0300

    add dmenu

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 echo "sfowm install script"
-echo "version 0.1"
+echo "version 0.2"
 case "$1" in
   "--deb")
-    sudo apt install picom
+    sudo apt install picom xsetroot dmenu 
   ;;
   "--arch")
-    sudo pacman -S picom
+    sudo pacman -S picom xorg-xsetroot dmenu 
 esac
 cp conf/picom.conf $HOME/.config/
 sudo make clean install
